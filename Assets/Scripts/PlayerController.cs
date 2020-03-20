dad3a0924cce37c2bfc4d84e6131b1279398a6d1
Riley Baker <vishnukey@gmail.com>
2020-03-19 19:54:25 -0600

    An example scene
    - Contains example material
    - Contains example character controller
    - contains some objects in the scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using /*import*/ UnityEngine;
+
+public class PlayerController : /*extends*/ MonoBehaviour
+{
+    /*
+     * public
+     * private (default)
+     * protected
+     */
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*GetComponent<COMPONET_TYPE>()*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            transform.position += -transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position += -transform.right * speed * Time.deltaTime;
+        }
+    }
+}
